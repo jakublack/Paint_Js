064cb86d39ede40e62cb0ede60fa0108eeaca916
Jakub Gawron <jakub.gawron@me.com>
2018-01-21 17:22:41 +0100

    add function to change the pen color and width

--- js/app.js
+++ js/app.js
@@ -17,6 +17,19 @@
             this.mouseDown = true;
         },
 
+        changePenColor: function(e) {
+            this.con.querySelector('.current').classList.remove('current');
+            e.target.classList.add('current');
+
+            this.ctx.strokeStyle = e.target.dataset.color;
+
+        },
+
+        changePenSize: function(penSize){
+
+            this.ctx.lineWidth = penSize;
+        },
+
         setupCanvas: function(){
 
             this.canvas.width = this.canvasCon.offsetWidth;
@@ -25,6 +38,15 @@
 
             this.ctx = this.canvas.getContext("2d");
 
+            this.ctx.fillStyle = '#fff';
+            this.ctx.fillRect(0,0,this.canvas.width, this.canvas.height);
+
+            this.ctx.lineWidth  =this.range.value;
+            this.ctx.lineJoin = "round";
+            this.ctx.lineCap = "round";
+            this.ctx.strokeStyle = this.con.querySelector('.current').dataset.color;
+
+        
             this.canvas.onmousemove = this.drawLines.bind(this);
             this.canvas.onmousedown = this.enableDrawing.bind(this);
             this.canvas.onmouseup = this.disableDrawing.bind(this);
@@ -36,12 +58,12 @@
             [].forEach.call(this.colors, function(color){
                 color.style.background = color.dataset.color;
 
-                // color.onclick = this.changePenColor.bind(this);
+                color.onclick = this.changePenColor.bind(this);
             }.bind(this));
 
             this.range.onchange = function(e) {
                 this.rangeOutput.innerHTML = e.target.value;
-                // this.changePenSize(e.target.value)
+                this.changePenSize(e.target.value)
             }.bind(this);
 
         },
